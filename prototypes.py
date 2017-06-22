378c635e4bcfe4081831ba2840457be381bf9e7b
Jorge Augusto da Costa Andrade <jacandrade@gmail.com>
2017-06-22 18:17:09 -0300

    added templating engine

---
+++ prototypes.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+from django.conf import settings
+
+BASE_DIR = os.path.dirname(__file__)
+settings.configure(
+        DEBUG=True,
+        SECRET_KEY='b0mqvak1p2sqm6p#+8o8fyxf+ox(le)8&jh_5^sxa!=7!+wxj0',
+        ROOT_URLCONF='sitebuilder.urls',
+        MIDDLEWARE_CLASSES=(),
+        INSTALLED_APPS=(
+            'django.contrib.staticfiles',
+            'sitebuilder',
+        ),
+        TEMPLATES = [
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS': [
+                    os.path.join(BASE_DIR, 'templates'),
+                ],
+                'APP_DIRS': True,
+                'OPTIONS': {
+                    'context_processors': [
+                    ],
+                },
+            },
+        ],
+        STATIC_URL='/static/',
+        SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR, 'pages'),
+)
+
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
